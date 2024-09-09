ee74f410656e244a40bf5123a1a00ac991d4e58e
Steve Lund <lund5000@gmail.com>
2024-09-09 09:31:07 -0400

    Update election dates

--- src/components/InfoPanel.vue
+++ src/components/InfoPanel.vue
@@ -218,14 +218,14 @@ export default {
       let html = '';
 
       if (selectedLocationType === 'polling-places') {
-        html = 'Vote in-person on Election Day: Tuesday, November 8 from 6:00 AM to 7:00 PM. As long as you are in line before 7:00 PM, you will still be able to vote. Voter identification is required. Find out more about <a href="https://azsos.gov/elections/voting-election">what you need to bring to vote in person</a>. <hr />At these locations, you can also drop off the ballot you received by mail.';
+        html = 'Vote in-person on Election Day: Tuesday, November 5 from 6:00 AM to 7:00 PM. As long as you are in line before 7:00 PM, you will still be able to vote. Voter identification is required. Find out more about <a href="https://azsos.gov/elections/voting-election">what you need to bring to vote in person</a>. <hr />At these locations, you can also drop off the ballot you received by mail.';
       } else if (selectedLocationType === 'early-voting-locations') {
-        html = 'Early voting ends has ended. If you cannot vote on November 8, you can vote on November 7 at an Emergency Voting Location.';
+        html = 'Early voting ends has ended. If you cannot vote on November 5, you can vote on November 4 at an Emergency Voting Location.';
       } else if (selectedLocationType === 'drop-boxes') {
-        html = 'Mail ballots can be returned at any drop box in your county until 7:00 PM on Election Day (Tuesday, November 8). You can also return them to any in-person voting locations during hours they are open.';
+        html = 'Mail ballots can be returned at any drop box in your county until 7:00 PM on Election Day (Tuesday, November 5). You can also return them to any in-person voting locations during hours they are open.';
       } else if (selectedLocationType === 'emergency-voting-locations') {
         html = `
-          On Monday, November 7, you can vote at
+          On Monday, November 4, you can vote at
           one of these emergency voting locations if something unexpected
           happens and you're no longer going to be able to vote on Election Day.
           Voter identification is required. Find out more about 
