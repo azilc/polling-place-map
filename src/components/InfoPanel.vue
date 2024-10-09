d3a230622dc8f04a5858cce2315992a316d283a4
Steve Lund <lund5000@gmail.com>
2024-10-09 10:37:34 -0400

    Add variable for early voting end date

--- src/components/InfoPanel.vue
+++ src/components/InfoPanel.vue
@@ -215,7 +215,7 @@ export default {
       if (selectedLocationType === 'polling-places') {
         html = `Vote in-person on Election Day: Tuesday, ${process.env.VUE_APP_ELECTION_DAY} from 6:00 AM to 7:00 PM. As long as you are in line before 7:00 PM, you will still be able to vote. Voter identification is required. Find out more about <a href="https://azsos.gov/elections/voting-election">what you need to bring to vote in person</a>. <hr />At these locations, you can also drop off the ballot you received by mail.`;
       } else if (selectedLocationType === 'early-voting-locations') {
-        html = `Early voting ends has ended. If you cannot vote on ${process.env.VUE_APP_ELECTION_DAY}, you can vote on ${process.env.VUE_APP_DAY_BEFORE_ELECTION_DAY} at an Emergency Voting Location.`;
+        html = `Early voting ends ${process.env.VUE_APP_EARLY_VOTING_END_DATE}. If you cannot vote on ${process.env.VUE_APP_ELECTION_DAY}, you can vote on ${process.env.VUE_APP_DAY_BEFORE_ELECTION_DAY} at an Emergency Voting Location.`;
       } else if (selectedLocationType === 'drop-boxes') {
         html = `Mail ballots can be returned at any drop box in your county until 7:00 PM on Election Day (Tuesday, ${process.env.VUE_APP_ELECTION_DAY}). You can also return them to any in-person voting locations during hours they are open.`;
       } else if (selectedLocationType === 'emergency-voting-locations') {
